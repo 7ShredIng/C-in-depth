063a95ccefafef9d5e2b61776059ac035d417a7a
7shreding <7shreding@gmail.com>
2017-12-18 23:52:19 +0100

    add menu to select subchapters in code examples. not doing anithing usefull at the moment.

--- Source/main.c
+++ Source/main.c
@@ -1,7 +1,27 @@
+#include <stdint.h>
 #include <stdio.h>
 
-int main()
+int32_t menu();
+
+int32_t main()
 {
-	printf("hi.\n");
+	int32_t choice = menu();
+	// do something with selected menu
+	printf("%d", choice);
 	return 0;
+}
+
+int32_t menu()
+{
+	printf("[0] Types, Operators, Expressions\n");
+	printf("[1] Control Flow\n");
+	printf("[2] Functions\n");
+	printf("[3] Pointers\n");
+	printf("[4] Arrays\n");
+	printf("---------------------------------\n");
+	printf("[q] Quit\n");
+	
+	int32_t option;
+	scanf("%d", &option, 1);
+	return option;
 }
